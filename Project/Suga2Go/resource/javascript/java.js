92d71d9c5a1990cd178156b528ef9ec5accf6c11
tsllex <NAlvinDesign@gmail.com>
2021-04-05 00:42:31 -0400

    Update java.js

--- Project/Suga2Go/resource/javascript/java.js
+++ Project/Suga2Go/resource/javascript/java.js
@@ -1,13 +1,13 @@
 var mainShow = document.querySelectorAll
 ('[data-component="mainSlideshow"]');
 
-mainShow.forEach(initSlideShow);
+mainShow.forEach(initMainSlideShow);
 
-function initSlideShow(mainSlideshow) {
+function initMainSlideShow(mainSlideshow) {
 	var slides = document.querySelectorAll
 	(`#${mainSlide.id} [role="list"] .mainSlides`);
 
-	var index = 0, time = 6000;
+	var index = 0, time = 11000;
 	slides[index].classList.add('active');
 	setInterval( () => {
 		slides[index].classList.remove('active');
@@ -19,3 +19,25 @@ function initSlideShow(mainSlideshow) {
 
 	}, time);
 	}
+
+var medShow = document.querySelectorAll
+('[data-component="medSlideshow"]');
+
+medShow.forEach(initMedSlideShow);
+
+function initMedSlideShow(medSlideshow) {
+	var slides = document.querySelectorAll
+	(`#${medPic.id} [role="list"] .medSlides`);
+
+	var index = 0, time = 13000;
+	slides[index].classList.add('active');
+	setInterval( () => {
+		slides[index].classList.remove('active');
+
+		index++;
+		if (index === slides.length) index = 0;
+
+		slides[index].classList.add('active');
+
+	}, time);
+}
